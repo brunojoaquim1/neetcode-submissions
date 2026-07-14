f98a8963fdf5bef712e02f468f64fb7b9a7da78b
Bruno Joaquim <52578899+brunojoaquim1@users.noreply.github.com>
2026-07-14 09:06:47 -0300

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        //early return 
+        if (s.length() != t.length()) return false;
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0 ; i < s.length(); i++){
+            if(map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i),map.get(s.charAt(i)) + 1 );
+            } else {
+                map.put(s.charAt(i), 1 );
+            }
+        }
+
+        for (int i = 0 ; i < t.length(); i++){
+            if(map.containsKey(t.charAt(i)) && map.get(t.charAt(i)) > 1 ){
+                map.put(t.charAt(i),map.get(t.charAt(i)) - 1 );
+            } else {
+                map.remove(t.charAt(i) , 1 );
+            }
+        }
+
+
+        return map.isEmpty();
+
+    }
+}
